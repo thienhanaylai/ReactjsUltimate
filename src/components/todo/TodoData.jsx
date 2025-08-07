b5449ce5e760234f69c96346f021a8c552e3e8a4
Paoo <thienhanaylai@gmail.com>
2025-08-07 17:20:02 +0700

    split component

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,11 @@
+const TodoData = () => {
+  return (
+    <>
+      <div className="todo-data">
+        <div>Learning react</div>
+      </div>
+    </>
+  );
+};
+
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,12 @@
+const TodoNew = () => {
+  return (
+    <>
+      <div className="todo-new">
+        <input type="text" name="" id="" />
+        <button>Add</button>
+      </div>
+    </>
+  );
+};
+
+export default TodoNew;
